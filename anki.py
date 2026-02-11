2568edab92d37c66ab54d5eab75728f420d44e9b
Austin Burdette <austinburdette5@gmail.com>
2026-02-11 17:39:12 -0500

    add models and start to structure functions

---
+++ anki.py
@@ -0,0 +1,34 @@
+import genanki
+
+
+def add_card_to_deck():
+    pass
+
+
+def create_deck():
+    pass
+
+
+# my_deck = genanki.Deck(2059400110, "My Custom Deck")
+#
+# my_model = genanki.Model(
+#     1607392319,
+#     "Simple Model",
+#     fields=[
+#         {"name": "Question"},
+#         {"name": "Answer"},
+#     ],
+#     templates=[
+#         {
+#             "name": "Card 1",
+#             "qfmt": "{{Question}}",
+#             "afmt": '{{FrontSide}}<hr id="answer">{{Answer}}',
+#         },
+#     ],
+# )
+#
+# my_deck.add_note(
+#     genanki.Note(model=my_model, fields=["What is sodium nitrite?", "NaNO₂"])
+# )
+#
+# genanki.Package(my_deck).write_to_file("my_deck.apkg")
