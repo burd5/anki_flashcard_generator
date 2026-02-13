a77747fbda9a0ea6458d7100a713d94685d178ad
Austin Burdette <austinburdette5@gmail.com>
2026-02-12 19:35:05 -0500

    parse kindle clippings file

--- data_sources/kindle.py
+++ data_sources/kindle.py
@@ -1,10 +1,31 @@
-def get_kindle_clippings():
-    pass
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+CLIPPINGS_FILE = os.getenv("PATH_TO_CLIPPINGS_FILE", None)
 
 
-def filter_clippings():
-    pass
+def get_kindle_clippings_file():
+    with open(CLIPPINGS_FILE) as f:
+        notes_and_highlights = f.read().split("==========")
+        highlights = [
+            highlight for highlight in notes_and_highlights if "Highlight" in highlight
+        ]
+        # notes = [note for note in notes_and_highlights if "Note" in note]
+        clip_highlights(highlights)
+
+
+def clip_highlights(highlights):
+    for highlight in highlights:
+        lines = highlight.split("\n")
+        title_and_author = lines[0] if lines[0] else lines[1]
+        high_text = lines[4]
+        print(title_and_author, high_text)
 
 
 def clear_clippings():
     pass
+
+
+get_kindle_clippings_file()
