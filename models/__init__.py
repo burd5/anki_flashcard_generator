443e9ce0f43dbe5fea8f5a8b1192efb141e01761
Austin Burdette <austinburdette5@gmail.com>
2026-02-13 13:21:51 -0500

    restructure repo

---
+++ models/__init__.py
@@ -0,0 +1,3 @@
+from models.Card import Card
+from models.Deck import Deck
+from models.Model import Model
--- services/kindle.py
+++ services/kindle.py
@@ -1,4 +1,7 @@
 from dotenv import load_dotenv
+from services.definition_api import get_word_definition
+from models import Card, Model, Deck
+from anki import add_card_to_deck
 import os
 
 load_dotenv()
@@ -20,8 +23,20 @@ def clip_highlights(highlights):
     for highlight in highlights:
         lines = highlight.split("\n")
         title_and_author = lines[0] if lines[0] else lines[1]
-        high_text = lines[4]
-        print(title_and_author, high_text)
+        highlighted_text = lines[4]
+
+        if len(highlighted_text.split()) == 1:
+            deck = Deck("vocab")
+            definition = get_word_definition(highlighted_text)
+            card = Card(highlighted_text, definition)
+            add_card_to_deck(deck, card)
+        else:
+            print(title_and_author)
+            deck = Deck(deck_name=title_and_author)
+            print(deck)
+            model = Model(model_name="Simple Model")
+            card = Card(question=highlighted_text, answer="")
+            add_card_to_deck(deck, model, card)
 
 
 def clear_clippings():
